219fd583ec61e8d2270f649655580fdaf9dc947e
SilverFox275 <113098236+SilverFox275@users.noreply.github.com>
2022-09-09 09:44:55 +0200

    Add files via upload

---
+++ function_frames_extraction.m
@@ -0,0 +1,11 @@
+function function_frames_extraction(Video,output_folder,frames)
+
+%this funciton reads the video and export the selected frames in format
+%.jpg in the created folder "Frames" inside the output folder
+
+mkdir(output_folder,'Frames') %creates a directory for selected frames
+for img = frames
+    filename = strcat(output_folder,'\Frames\\frame',num2str(img),'.jpg');
+    b = read(Video, img);
+    imwrite(b, filename);
+end
---
+++ manual_point_tracking.m
@@ -0,0 +1,38 @@
+% clear the workspace and command window
+clc
+clear all
+
+%% SET INPUT PARAMETERS
+% Here are all the settings for the tracking
+
+video_folder="C:\Users\d067879\Videos\1C"; %folder with (!ONLY!) the videos to track
+video_name='Example.mp4'; %video name
+n_object=6; %number of objects to track in each frame
+track_start=30; % starting point to track [seconds]
+track_n=2; % sampling size [frames]
+track_fps=10; %sampling frequency [frames per seconds]
+
+%from now on, the code should not be touched!
+
+%% VARIABLE DEFINITION
+
+Video = VideoReader(strcat(video_folder,'\',video_name)); %reading of the video
+fps=Video.FrameRate; %frame rate of the video
+frames=int16(linspace(track_start*fps,(track_start+(track_n-1)/track_fps)*fps,track_n));
+
+output_folder=strcat(video_name(1:end-4),'_track');
+
+%% FRAMES EXTRACTION
+function_frames_extraction(Video,output_folder,frames)
+
+%% IDENTIFYING THE CORNERS
+%order of clicking:
+% 1: TOP LEFT
+% 2: BOTTOM LEFT
+% 3: BOTTOM RIGHT
+% 4: TOP RIGHT
+
+function_write_corners(output_folder,video_name,frames(1))
+
+%% MANUAL TRACKING
+function_point_tracking(output_folder,video_name,frames,n_object)
