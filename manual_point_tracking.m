a692f9ca3becea2d2cc53694a23876494ad5edab
SilverFox275 <113098236+SilverFox275@users.noreply.github.com>
2022-09-09 14:19:36 +0200

    Add files via upload

--- manual_point_tracking.m
+++ manual_point_tracking.m
@@ -2,7 +2,7 @@
 clc
 clear all
 
-%% SET INPUT PARAMETERS
+% SET INPUT PARAMETERS
 % Here are all the settings for the tracking
 
 video_folder="C:\Users\d067879\Videos\1C"; %folder with (!ONLY!) the videos to track
@@ -13,7 +13,7 @@ track_n=2; % sampling size [frames]
 track_fps=10; %sampling frequency [frames per seconds]
 
 %from now on, the code should not be touched!
-
+%---------------------------------------------------------------------------------------------------------
 %% VARIABLE DEFINITION
 
 Video = VideoReader(strcat(video_folder,'\',video_name)); %reading of the video
@@ -22,10 +22,15 @@ frames=int16(linspace(track_start*fps,(track_start+(track_n-1)/track_fps)*fps,tr
 
 output_folder=strcat(video_name(1:end-4),'_track');
 
+% ------------------------------------------------------------------------------------------------------------------------
 %% FRAMES EXTRACTION
+
 function_frames_extraction(Video,output_folder,frames)
 
-%% IDENTIFYING THE CORNERS
+%------------------------------------------------------------------------------------------------------------------------------------
+%% DELIMITING TRACKING REGION
+
+% IDENTIFYING THE CORNERS
 %order of clicking:
 % 1: TOP LEFT
 % 2: BOTTOM LEFT
@@ -34,13 +39,18 @@ function_frames_extraction(Video,output_folder,frames)
 
 function_write_corners(output_folder,video_name,frames(1))
 
+%--------------------------------------------------------------------------------------------------------------------------------------
 %% MANUAL TRACKING
+
 function_point_tracking(output_folder,video_name,frames,n_object)
 
+%--------------------------------------------------------------------------------------------------------------------------
 %% CONVERSION INTO THE NEW REFERENCE SYSTEM
-corner_csv_name=strcat('corners_',num2str(video_name(1:end-4)),'.csv');
 
+corner_csv_name=strcat('corners_',num2str(video_name(1:end-4)),'.csv');
 corners=table2array(readtable(strcat(output_folder,'\',corner_csv_name)));
 xy=table2array(readtable(strcat(output_folder,'\tracking_',video_name(1:end-4),'.csv')));
 
 function_reference_system_change(xy,output_folder,video_name,corners,n_object)
+
+%---------------------------------------------------------------------------------------------------------------------
